7b8e565e9e3d3e52b4e3d1ded561bd735245d2d3
alt-Pranav <74000887+alt-Pranav@users.noreply.github.com>
2022-09-15 15:43:26 +0530

    #8, installed pytesseract on venv, testOCR

---
+++ testOCR.py
@@ -0,0 +1,71 @@
+import sys
+from PIL import Image
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+import cv2
+import pytesseract
+import numpy
+
+pytesseract.pytesseract.tesseract_cmd="C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.VBL = QVBoxLayout()
+
+        self.FeedLabel = QLabel()
+        self.VBL.addWidget(self.FeedLabel)
+
+        self.textLabel = QLabel()
+        self.textLabel.text = "N/A"
+        self.VBL.addWidget(self.textLabel)
+
+        self.CancelBTN = QPushButton("Cancel")
+        self.CancelBTN.clicked.connect(self.CancelFeed)
+        
+        self.VBL.addWidget(self.CancelBTN)
+
+        self.Worker1 = Worker1()
+
+        self.Worker1.start()
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+        self.setLayout(self.VBL)
+
+    def ImageUpdateSlot(self, Pic, img):
+        self.FeedLabel.setPixmap(QPixmap.fromImage(Pic))
+        self.textLabel.setText(pytesseract.image_to_string(img))
+        print(self.textLabel.text)
+
+    def CancelFeed(self):
+        self.Worker1.stop()
+
+class Worker1(QThread):
+    ImageUpdate = pyqtSignal(QImage, numpy.ndarray)
+    def run(self):
+        self.ThreadActive = True
+        Capture = cv2.VideoCapture(0)
+
+        while self.ThreadActive:
+            ret, frame = Capture.read()
+            if ret:
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImage = cv2.flip(img, 1)
+                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
+
+                self.ImageUpdate.emit(Pic, img)
+
+        Capture.release()
+
+    def stop(self):
+        self.ThreadActive = False
+        
+        self.quit()
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    Root = MainWindow()
+    Root.show()
+    sys.exit(App.exec())
